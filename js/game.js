739241ef234c72c78a8dd53a004d6f81549cb45c
Alex <ammorishita@gmail.com>
2016-10-16 17:06:40 -0700

    Add some menu UI and interface animation.

--- js/game.js
+++ js/game.js
@@ -1,18 +1,22 @@
 var BoxStats = [
 	{
-		'score': 1
+		'round': 1,
+		'level': 1
 	}
 ];
 var Box = function(data) {
-	this.score = ko.observable(data.score);
+	this.round = ko.observable(data.round);
+	this.level = ko.observable(data.level);
 };
 var ViewModel = function() {
 	var self = this;
 	var width = height = 200;
-	var speed = 21000;
+	var speed = 2000;
 	var number;
+	var distance = 0;
 	$('#screen').css('pointer-events', 'none');
 	self.dontclick = ko.observable(false);
+	self.started = ko.observable(false);
 	self.level1 = ko.observable(false);
 	self.level2 = ko.observable(false);
 	self.level3 = ko.observable(false);
@@ -29,6 +33,15 @@ var ViewModel = function() {
 		console.log(self.boxlist());
 	});
 	self.currentBox = ko.observable(self.boxlist()[0]);
+	/// ---- ALL MENU FUNCTIONS --- ///
+	$('.gameplay').click(function() {
+		$('#gameplay').toggle()	
+	})
+
+
+
+
+
 
 	////////--------ALL GAME FUNCTIONS-------------////////
 	self.randomGenerate = function() {
@@ -36,15 +49,16 @@ var ViewModel = function() {
 		console.log(number);
 	};
 	self.start = function() {
+		self.started(true);
 		self.choice();
 		self.win(false);
 		self.level1(true);
 		console.log(speed);	
 		console.log(number);
 		console.log($('#box').position());
-		$('.start').hide();
 		self.gameover(false);
 		$('#screen').css('pointer-events', 'auto');
+		$('#box').css('display', 'block');
 		box.addEventListener('click', self.success);
 	};
 	self.choice = function() {
@@ -54,55 +68,56 @@ var ViewModel = function() {
 		var xPos = box.position().left;		
 		console.log(box.position());
 		self.randomGenerate();
-		if (number < 0.5 && xPos > 805 && yPos < 178) {
+		if (number < 0.5 && xPos >= 500 && yPos < 178) {
 			self.left();
 			console.log('moved left from top right')
-		} else if (number >= 0.5 && xPos > 805 && yPos < 178) {
+		} else if (number >= 0.5 && xPos >= 500 && yPos < 178) {
 			self.down();
 			console.log('moved down from top right')
-		} else if (number >= 0.5 && xPos > 805 && yPos > 178) {
+		} else if (number >= 0.5 && xPos >= 500 && yPos >= 178) {
 			self.left();
 			console.log('moved left from bottom right')
-		} else if (number < 0.5 && xPos > 805 && yPos > 178) {
+		} else if (number < 0.5 && xPos >= 500 && yPos >= 178) {
 			self.up();
 			console.log('moved up from bottom right')
-		} else if (number >= 0.5 && xPos < 656 && yPos > 178) {
+		} else if (number >= 0.5 && xPos < 500 && yPos >= 178) {
 			self.right();
 			console.log('moved right from bottom left')
-		} else if (number < 0.5 && xPos < 656 && yPos > 178) {
+		} else if (number < 0.5 && xPos < 500 && yPos >= 178) {
 			self.up();
 			console.log('moved up from bottom left')
-		} else if (number >= 0.5 && xPos < 656 && yPos < 178) {
+		} else if (number >= 0.5 && xPos < 500 && yPos < 178) {
 			self.right();
 			console.log('moved right from top left')
-		} else if (number < 0.5 && xPos < 656 && yPos < 178) {
+		} else if (number < 0.5 && xPos < 500 && yPos < 178) {
 			self.down();
 			console.log('moved down from top left')
 		}
 	};
 	self.right = function() {
 		$('#box').animate({
-			left: "500px"
+			left: 500 + distance + 'px'
 		}, speed, self.choice);
 	};
 	self.left = function() {
 		$('#box').animate({
-			left: "0px"
+			left: '0px'
 		}, speed, self.choice);
 	};
 	self.down = function() {
 		$('#box').animate({
-			top: "260px"
+			top: 260 + distance + 'px'
 		}, speed, self.choice);
 	};
 	self.up = function() {
 		$('#box').animate({
-			top: "0px"
+			top: '0px'
 		}, speed, self.choice);
 	};
 	self.success = function(e) {
+		distance+=25;
 		e.stopPropagation();
-		self.currentBox().score(self.currentBox().score() + 1);
+		self.currentBox().round(self.currentBox().round() + 1);
 		$('#box').stop();
 		$('#box').css('background' , 'red');
 		$('#screen').css('pointer-events', 'none')
@@ -111,20 +126,21 @@ var ViewModel = function() {
 			top: '0px',
 			left: '0px'
 		}, 2000, self.nextLevel);
-		var score = self.currentBox().score();
-		console.log('current score' + score);
-		if(score == 2) {self.level2(true); self.level1(false);}
-		if(score == 3) {self.level3(true); self.level2(false);}
-		if(score == 4) {self.level4(true); self.level3(false);}
-		if(score == 5) {self.level5(true); self.level4(false);}
-		if(score == 6) {self.level6(true); self.level5(false);}
-		if(score == 7) {self.level7(true); self.level6(false);}
-		if(score == 8) {self.level8(true); self.level7(false);}	
-		if(score == 9) {
+		var round = self.currentBox().round();
+		console.log('current round' + round);
+		if(round == 2) {self.level2(true); self.level1(false);}
+		if(round == 3) {self.level3(true); self.level2(false);}
+		if(round == 4) {self.level4(true); self.level3(false);}
+		if(round == 5) {self.level5(true); self.level4(false);}
+		if(round == 6) {self.level6(true); self.level5(false);}
+		if(round == 7) {self.level7(true); self.level6(false);}
+		if(round == 8) {self.level8(true); self.level7(false);}	
+		if(round == 9) {
 			self.win(true); 
 			self.level8(false);
 			self.reset();
 			self.dontclick(false);
+			self.currentBox().level(self.currentBox().level() + 1);
 		}	
 	};
 	self.nextLevel = function() {
@@ -158,10 +174,13 @@ var ViewModel = function() {
 
 	};
 	self.reset = function() {
-		self.currentBox().score(1);
+		self.currentBox().round(1);
+		self.currentBox().level(1);
+		self.started(false);
 		width = 200;
 		height = 200;
 		speed = 2000;
+		distance = 0;
 		self.level1(false);
 		self.level2(false);
 		self.level3(false);
@@ -175,7 +194,8 @@ var ViewModel = function() {
 			top: '0px',
 			width: '200px',
 			height: '200px',
-			backgroundColor: 'purple'
+			backgroundColor: 'purple',
+			display: 'none'
 		});
 		box.removeEventListener('click', self.success);
 		$('.start').show();
